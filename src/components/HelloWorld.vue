91640f9adb163a0db81abd07564c6be6f107c2aa
unknown <1909590616@qq.com>
2019-12-27 16:02:48 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="views">
+    <mapshow/>
+  </div>
+</template>
+
+<script>
+import mapshow from './mapshow'
+export default {
+  name: 'HelloWorld',
+  components:{
+    mapshow
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "@/styles/global.scss";
+.views {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  width: vw(1920);
+  height: vh(1080);
+  }
+
+</style>
